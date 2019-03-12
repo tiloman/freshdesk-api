37c11e2b68c408a2d0f365fe83c70efae40d25d2
Johannes Heck <yoyostile@gmail.com>
2019-03-12 14:09:50 +0100

    fix company -> tickets relation

--- lib/freshdesk/api/company.rb
+++ lib/freshdesk/api/company.rb
@@ -4,6 +4,7 @@ module Freshdesk
   module Api
     class Company < Base
       has_many :contacts, class_name: 'Freshdesk::Api::Contact'
+      has_many :tickets, class_name: 'Freshdesk::Api::Ticket'
     end
   end
 end
