d3d1c7abae021669214c65f8f624bccb86dbd4f9
Lars Brillert <lars@railslove.com>
2020-02-11 09:45:48 +0100

    partial updates FTW

--- lib/freshdesk/api/base.rb
+++ lib/freshdesk/api/base.rb
@@ -11,6 +11,12 @@ module Freshdesk
         end
       end
 
+      def partial_update(attributes)
+        connection.put(element_path(prefix_options), encode(attributes), self.class.headers).tap do |response|
+          load_attributes_from_response(response)
+        end
+      end
+
       def as_json(opts = {})
         super({ except: %i[id facebook_id created_at updated_at] }.merge(opts))
       end
